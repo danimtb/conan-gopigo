259c80aa9e27a7d3e82f19e29bd00da7eae90d53
czoido <mrgalleta@gmail.com>
2019-08-24 20:22:51 +0200

    back to build.py

--- .travis/run.sh
+++ .travis/run.sh
@@ -3,4 +3,11 @@
 set -e
 set -x
 
-conan create . gopigo/stable
+if [[ "$(uname -s)" == 'Darwin' ]]; then
+    if which pyenv > /dev/null; then
+        eval "$(pyenv init -)"
+    fi
+    pyenv activate conan
+fi
+
+python build.py
